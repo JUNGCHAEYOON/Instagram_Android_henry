40f3645ad0d5e6277cb818f727449837bd290fd9
JUNGCHAEYOON <108243290+JUNGCHAEYOON@users.noreply.github.com>
2023-03-27 19:06:53 +0900

    0327 commit
    메인화면 바텀네비게이션 구성, 전화번호 로그인 구현중

--- app/src/main/java/com/example/instagram_android_henry/src/login/LoginActivity.kt
+++ app/src/main/java/com/example/instagram_android_henry/src/login/LoginActivity.kt
@@ -1,12 +1,29 @@
 package com.example.instagram_android_henry.src.login
 
+import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
 import com.example.instagram_android_henry.config.BaseActivity
 import com.example.instagram_android_henry.databinding.ActivityLoginBinding
+import com.example.instagram_android_henry.src.main.MainActivity
 
 class LoginActivity : BaseActivity<ActivityLoginBinding>(ActivityLoginBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // 내계정
+        binding.loginBtnMyaccount.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+
+        // 다른 계정으로 로그인
+        binding.loginBtnAnotheraccount.setOnClickListener {
+            showCustomToast("다른 계정으로 로그인!")
+        }
+
+        // 새계정 만들기
+        binding.loginBtnNewaccount.setOnClickListener {
+            showCustomToast("새 계정 만들기!")
+        }
     }
 }
--- app/src/main/java/com/example/instagram_android_henry/src/main/MainActivity.kt
+++ app/src/main/java/com/example/instagram_android_henry/src/main/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.instagram_android_henry.src.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -10,6 +11,34 @@ import com.example.instagram_android_henry.databinding.ActivityMainBinding
 class MainActivity : BaseActivity<ActivityMainBinding>(ActivityMainBinding::inflate){
     override fun onCreate(savedInstanceState: Bundle?,) {
         super.onCreate(savedInstanceState)
+
+        //add버튼은 액티비티 이동 왼쪽으로
+//        val intentAdd = Intent(this,AddActivity::class.java)
+
+        //바텀네비 클릭
+        binding.mainBnv.run{
+            setOnItemSelectedListener { item ->
+                when(item.itemId){
+                    R.id.main_btn_nav_home->{
+                        showCustomToast("홈")
+                    }
+                    R.id.main_btn_nav_search->{
+                        showCustomToast("검색")
+                    }
+                    R.id.main_btn_nav_add->{
+                        showCustomToast("글쓰기")
+                    }
+                    R.id.main_btn_nav_reals->{
+                        showCustomToast("릴스")
+                    }
+                    R.id.main_btn_nav_profile->{
+                        showCustomToast("프로필")
+                    }
+                }
+                true
+            }
+            selectedItemId = R.id.main_btn_nav_home
+        }
     }
 }
 
